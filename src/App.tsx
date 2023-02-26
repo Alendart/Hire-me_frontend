42034e6848bb201ffd02e838eb2619114ddde69f
Alendart <111238215+Alendart@users.noreply.github.com>
2023-02-26 14:47:45 +0100

    Added Routes and views for Map and single apply

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import {MainView} from "./component/views/MainView";
 import {Route, Routes} from "react-router-dom";
+import {ApplyView} from "./component/views/ApplyView";
+import {MapView} from "./component/views/MapView";
 
 function App() {
-  return (
-      <>
-          <Routes>
-              <Route path="/" element={<MainView/>}/>
-              <Route path="*" element={<MainView/>}/>
-          </Routes>
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<MainView/>}/>
+                <Route path="/map" element={<MapView/>}/>
+                <Route path="/apply/:id" element={<ApplyView/>}/>
+                <Route path="*" element={<MainView/>}/>
+            </Routes>
 
       </>
   );
