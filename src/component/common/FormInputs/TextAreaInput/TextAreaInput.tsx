e094aa9a321ccefd3e3443c96cc01cb15655fd11
Alendart <111238215+Alendart@users.noreply.github.com>
2023-03-17 19:41:37 +0100

    Moving to another workspace

---
+++ src/component/common/FormInputs/TextAreaInput/TextAreaInput.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./TextAreaInput.css"
+
+interface Props {
+    label: string
+    name: string;
+    type: string;
+    placeholder: string;
+    children: JSX.Element
+}
+
+export const TextAreaInput = (props: Props) => {
+
+    return (
+        <></>
+    )
+}
