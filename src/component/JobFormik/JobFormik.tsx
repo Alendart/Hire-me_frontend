bf5d1ad680c32057570456970595e7f5b260eb69
Pawel Baraniak <111238215+Alendart@users.noreply.github.com>
2023-03-18 23:07:56 +0100

    Added validation to Formik, some changes in css

--- src/component/JobFormik/JobFormik.tsx
+++ src/component/JobFormik/JobFormik.tsx
@@ -20,12 +20,19 @@ export const JobFormik = () => (
                 }}
             validationSchema={Yup.object({
                 jobName: Yup.string()
-                    .required("Pole wymagane"),
-                jobDesc: Yup.string(),
+                    .required("Pole wymagane")
+                    .min(5, "Nazwa musi mieć minimum 5 znaków")
+                    .max(30, "Nazwa nie może mieć więcej niż 30 znaków"),
+                jobDesc: Yup.string()
+                    .required("Pole wymagane")
+                    .min(1, "Opis nie może być pusty")
+                    .max(10000, "Opis nie może przekraczać 10 tysięcy znaków"),
                 url: Yup.string()
-                    .required("Pole wymagane"),
+                    .required("Pole wymagane")
+                    .max(200, "Url nie może mieć więcej niż 200 znaków"),
                 address: Yup.string()
-                    .required("Pole wymagane"),
+                    .required("Pole wymagane")
+                    .max(100, "Adres nie może mieć więcej niż 100 znaków")
             })}
             onSubmit={(
                 values: JobValues,
