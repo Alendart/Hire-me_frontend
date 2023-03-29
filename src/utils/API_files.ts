5a17730f62c658e694e9017fc921b9d910851d1a
Alendart <111238215+Alendart@users.noreply.github.com>
2023-03-30 00:49:57 +0200

    Prepared file for connections with API - for file handling

---
+++ src/utils/API_files.ts
@@ -0,0 +1,25 @@
+import {ErrorMessage} from "types";
+import {apiUrl} from "../config/api_config";
+
+
+const fileUrl = `${apiUrl}/cv`
+
+export async function addFile(data: File,fileName: string,jobId: string): Promise<true | ErrorMessage | any> {
+    try {
+        const formData = new FormData();
+        formData.append('file',data)
+        formData.append('name',fileName)
+        formData.append('jobId',jobId)
+
+        const res = await fetch(`${fileUrl}/`,{
+            method: "POST",
+            credentials: "include",
+            body: formData,
+        })
+
+        return await res.json();
+    } catch (e) {
+        return e
+    }
+
+}
